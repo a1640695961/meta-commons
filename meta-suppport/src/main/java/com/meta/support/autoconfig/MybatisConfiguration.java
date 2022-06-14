808e5051a9dd8bd2679586bc3481ebcf5fa51269
zhad <1640695961@qq.com>
2022-06-14 23:14:00 +0800

    mybatis config

---
+++ meta-suppport/src/main/java/com/meta/support/autoconfig/MybatisConfiguration.java
@@ -0,0 +1,27 @@
+package com.meta.support.autoconfig;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author zhad
+ * @date 2022年06月14日 23:12
+ */
+@Configuration
+public class MybatisConfiguration {
+    private static final Logger log = LoggerFactory.getLogger(MybatisConfiguration.class);
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return interceptor;
+    }
+
+    public MybatisConfiguration() {
+    }
+}
